58c842cba559a5008daaef3f17daa5b46abca5a7
Maximilian Albert <maximilian.albert@gmail.com>
2019-03-17 23:53:58 +0000

    Refactor freqs classes

--- chantstats/chantstats/pitch_class_freqs.py
+++ chantstats/chantstats/pitch_class_freqs.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 __all__ = ["PCFreqs"]
 
-ALLOWED_PITCH_CLASSES = ["A", "B-", "B", "C", "D", "E", "F", "G"]
-
 
 class classproperty:
     def __init__(self, f):
@@ -22,8 +20,24 @@ class BaseFreqsMeta(type):
 
 
 class BaseFreqs(metaclass=BaseFreqsMeta):
+    """
+    Base class representing absolute/relative frequencies of some entity
+    (for example, of pitch classes). This base class provides all the
+    machinery; derived classes only need to set ALLOWED_VALUES.
+    """
 
-    abs_freqs = None
+    ALLOWED_VALUES = []  # derived classes need to set this to a list of allowed values
+
+    def __init__(self, list_or_freqs):
+        if isinstance(list_or_freqs, pd.Series):
+            freqs = list_or_freqs
+            assert list(freqs.index == self.ALLOWED_VALUES)
+            self.abs_freqs = freqs
+        elif isinstance(list_or_freqs, list):
+            pitch_classes = list_or_freqs
+            self.abs_freqs = pd.Series(Counter(pitch_classes), index=self.ALLOWED_VALUES).fillna(0, downcast="infer")
+        else:
+            raise ValueError(f"Cannot instantiate {self.__class__.__name__} from object: {list_or_freqs}")
 
     def __add__(self, other):
         assert isinstance(other, self.__class__)
@@ -39,16 +53,11 @@ class BaseFreqs(metaclass=BaseFreqsMeta):
 
 
 class PCFreqs(BaseFreqs):
-    def __init__(self, pitch_classes_or_freqs):
-        if isinstance(pitch_classes_or_freqs, pd.Series):
-            freqs = pitch_classes_or_freqs
-            assert list(freqs.index == ALLOWED_PITCH_CLASSES)
-            self.abs_freqs = freqs
-        elif isinstance(pitch_classes_or_freqs, list):
-            pitch_classes = pitch_classes_or_freqs
-            self.abs_freqs = pd.Series(Counter(pitch_classes), index=ALLOWED_PITCH_CLASSES).fillna(0, downcast="infer")
-        else:
-            raise ValueError(f"Cannot instantiate PCFreqs from object: {pitch_classes_or_freqs}")
+    """
+    Represent pitch class frequencies.
+    """
+
+    ALLOWED_VALUES = ["A", "B-", "B", "C", "D", "E", "F", "G"]
 
 
 def calculate_pc_freqs_for_collection_of_phrases(phrases):
