0cc318e0b08aee4e25135c2838b1e83fbbb0cda5
Maximilian Albert <maximilian.albert@gmail.com>
2019-03-23 13:57:09 +0000

    Add logger

--- chantstats/chantstats/__init__.py
+++ chantstats/chantstats/__init__.py
@@ -1,3 +1,8 @@
+from .logging import logger
+
+chantstats_logger = logger  # alias for convenience
+
+# Set __version__ attribute (via versioneer)
 from ._version import get_versions
 
 __version__ = get_versions()["version"]
