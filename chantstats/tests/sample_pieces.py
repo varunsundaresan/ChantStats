3ae67b0f3a720fd1e563148a2248177e7c0e76cc
Maximilian Albert <maximilian.albert@gmail.com>
2019-03-23 15:24:17 +0000

    Use helper function to load sample pieces 1-8

--- chantstats/tests/sample_pieces.py
+++ chantstats/tests/sample_pieces.py
@@ -1,4 +1,5 @@
 import os
+from chantstats import load_plainchant_sequence_pieces
 from chantstats.piece import PlainchantSequencePiece
 
 try:
@@ -9,14 +10,16 @@ except KeyError:
 #
 # Load sample pieces
 #
-piece1 = PlainchantSequencePiece(os.path.join(chants_dir, "01_Salus_eterna.xml"))       # monomodal frame (no amen formula)
-piece2 = PlainchantSequencePiece(os.path.join(chants_dir, "02_Regnantem.xml"))          # monomodal frame (with amen formula)
-piece3 = PlainchantSequencePiece(os.path.join(chants_dir, "03_Qui_regis.xml"))          # heavy polymodal frame 1 (no amen formula)
-piece4 = PlainchantSequencePiece(os.path.join(chants_dir, "04_Iubilemus_omnes.xml"))    # heavy polymodal frame 1 (no amen formula)
-piece5 = PlainchantSequencePiece(os.path.join(chants_dir, "05_Letabundus.xml"))         # monomodal frame (with amen formula)
-piece6 = PlainchantSequencePiece(os.path.join(chants_dir, "06_Hac_clara_die.xml"))      # heavy polymodal frame 1 (no amen formula)
-piece7 = PlainchantSequencePiece(os.path.join(chants_dir, "07_Splendor_patris.xml"))    # monomodal frame (with amen formula)
-piece8 = PlainchantSequencePiece(os.path.join(chants_dir, "08_In_excelsis_canitur.xml"))  # monomodal frame (without amen formula)
+pieces_1_thru_8 = load_plainchant_sequence_pieces(chants_dir, pattern="0[1-8]*.xml")
+# piece1 = PlainchantSequencePiece(os.path.join(chants_dir, "01_Salus_eterna.xml"))       # monomodal frame (no amen formula)
+# piece2 = PlainchantSequencePiece(os.path.join(chants_dir, "02_Regnantem.xml"))          # monomodal frame (with amen formula)
+# piece3 = PlainchantSequencePiece(os.path.join(chants_dir, "03_Qui_regis.xml"))          # heavy polymodal frame 1 (no amen formula)
+# piece4 = PlainchantSequencePiece(os.path.join(chants_dir, "04_Iubilemus_omnes.xml"))    # heavy polymodal frame 1 (no amen formula)
+# piece5 = PlainchantSequencePiece(os.path.join(chants_dir, "05_Letabundus.xml"))         # monomodal frame (with amen formula)
+# piece6 = PlainchantSequencePiece(os.path.join(chants_dir, "06_Hac_clara_die.xml"))      # heavy polymodal frame 1 (no amen formula)
+# piece7 = PlainchantSequencePiece(os.path.join(chants_dir, "07_Splendor_patris.xml"))    # monomodal frame (with amen formula)
+# piece8 = PlainchantSequencePiece(os.path.join(chants_dir, "08_In_excelsis_canitur.xml"))  # monomodal frame (without amen formula)
+piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8 = pieces_1_thru_8
 piece12 = PlainchantSequencePiece(os.path.join(chants_dir, "12_Fulgens_preclara.xml"))  # heavy polymodal frame 1 (no amen formula)
-piece14 = PlainchantSequencePiece(os.path.join(chants_dir, "14_Zima_uetus.xml"))        # light polymodal frame 1 (with amen formula)
+piece14 = PlainchantSequencePiece(os.path.join(chants_dir, "14_Zima_uetus.xml"))  # light polymodal frame 1 (with amen formula)
 piece45 = PlainchantSequencePiece(os.path.join(chants_dir, "45_Aue_maria_gratia_plena.xml"))  # light polymodal frame 2 (with amen formula)
