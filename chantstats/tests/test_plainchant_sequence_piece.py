de810916a29ad9d38b3dafc6885961e7340a78de
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-19 23:05:55 +0100

    Fix tests

--- chantstats/tests/test_plainchant_sequence_piece.py
+++ chantstats/tests/test_plainchant_sequence_piece.py
@@ -10,14 +10,21 @@ except KeyError:
 
 @pytest.mark.slow
 def test_load_plainchant_sequence_pieces():
-    filename_pattern = "3[1-3]*.xml"
+    filename_pattern = "BN_lat_1112_Sequence_3[1-3]*.xml"
     pieces = load_plainchant_sequence_pieces(chants_dir, pattern=filename_pattern)
-    pieces_expected = ["31_Heri_mundus_exultauit.xml", "32_Magnus_deus.xml", "33_Gratulemur_ad_fastiuum.xml"]
+    pieces_expected = [
+        "BN_lat_1112_Sequence_31_Heri_mundus_exultauit.xml",
+        "BN_lat_1112_Sequence_32_Magnus_deus.xml",
+        "BN_lat_1112_Sequence_33_Gratulemur_ad_fastiuum.xml",
+    ]
     assert pieces_expected == [p.filename_short for p in pieces]
 
     # When excluding piece with heavy polymodal frames we expect `32_Magnus_deus.xml` to be missing
     pieces = load_plainchant_sequence_pieces(
         chants_dir, pattern=filename_pattern, exclude_heavy_polymodal_frame_pieces=True
     )
-    pieces_expected = ["31_Heri_mundus_exultauit.xml", "33_Gratulemur_ad_fastiuum.xml"]
+    pieces_expected = [
+        "BN_lat_1112_Sequence_31_Heri_mundus_exultauit.xml",
+        "BN_lat_1112_Sequence_33_Gratulemur_ad_fastiuum.xml",
+    ]
     assert pieces_expected == [p.filename_short for p in pieces]
--- chantstats/tests/test_results_export.py
+++ chantstats/tests/test_results_export.py
@@ -5,29 +5,38 @@ from chantstats import FullAnalysisSpec
 
 
 @pytest.mark.parametrize(
-    "repertoire_and_genre, analysis, unit, mode, output_path_expected",
+    "repertoire_and_genre, analysis, unit, mode, final, output_path_expected",
     [
-        ("plainchant_sequences", "pc_freqs", "pcs", "by_final", "/foobar/chant/pc_freqs/sequences/pcs/by_final"),
+        (
+            "plainchant_sequences",
+            "pc_freqs",
+            "pcs",
+            "by_final",
+            "D",
+            "/foobar/chant/pc_freqs/sequences/pcs/by_final/D_final",
+        ),
         (
             "responsorial_chants",
             "tendency",
             "mode_degrees",
             "authentic_modes",
-            "/foobar/chant/tendency/responsorial_chants/mode_degrees/authentic_modes",
+            "G",
+            "/foobar/chant/tendency/responsorial_chants/mode_degrees/authentic_modes/G_authentic",
         ),
         (
             "organa",
             "approaches_and_departures",
             "pcs",
             "plagal_modes",
-            "/foobar/organum/approaches_and_departures/pcs/plagal_modes",
+            "C",
+            "/foobar/organum/approaches_and_departures/pcs/plagal_modes/C_plagal",
         ),
     ],
 )
-def test_output_path(repertoire_and_genre, analysis, unit, mode, output_path_expected):
+def test_output_path(repertoire_and_genre, analysis, unit, mode, final, output_path_expected):
     """
     Analysis spec returns the expected output path, depending on its input parameters.
     """
     spec = FullAnalysisSpec(repertoire_and_genre=repertoire_and_genre, analysis=analysis, unit=unit, mode=mode)
-    output_path = spec.output_path(root_dir="/foobar")
+    output_path = spec.output_path(root_dir="/foobar", final=final)
     assert output_path_expected == output_path
