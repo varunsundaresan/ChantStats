f43ae00a24f576be63c17e1ba3c6fae54de66832
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-20 08:17:12 +0100

    Add test that the output folder structure is as expected.

--- chantstats/tests/test_results_export.py
+++ chantstats/tests/test_results_export.py
@@ -1,7 +1,15 @@
+import os
 import pytest
 
 from .context import chantstats
-from chantstats import FullAnalysisSpec
+from chantstats import load_plainchant_sequence_pieces, FullAnalysisSpec, GroupingByModalCategory
+from chantstats.utils import list_directory_tree
+
+
+try:
+    chants_dir = os.environ["CHANTS_DIR"]
+except KeyError:
+    raise RuntimeError("The environment variable CHANTS_DIR must be defined to run the tests.")
 
 
 @pytest.mark.parametrize(
@@ -40,3 +48,24 @@ def test_output_path(repertoire_and_genre, analysis, unit, mode, final, output_p
     spec = FullAnalysisSpec(repertoire_and_genre=repertoire_and_genre, analysis=analysis, unit=unit, mode=mode)
     output_path = spec.output_path(root_dir="/foobar", final=final)
     assert output_path_expected == output_path
+
+
+@pytest.mark.slow
+def test_results_folder_structure(tmpdir):
+    output_root_dir = str(tmpdir)
+    print(f"[DDD] output_root_dir={output_root_dir}")
+    pieces = load_plainchant_sequence_pieces(chants_dir, pattern="*.xml", exclude_heavy_polymodal_frame_pieces=False)
+    monomodal_sections = sum([p.get_monomodal_sections(min_length=3) for p in pieces], [])
+    analysis_spec = FullAnalysisSpec(
+        repertoire_and_genre="plainchant_sequences", analysis="pc_freqs", unit="pcs", mode="authentic_modes"
+    )
+    group_by = "final"
+    grouping = GroupingByModalCategory(monomodal_sections, group_by=group_by)
+    analysis_spec.export_results(
+        output_root_dir=output_root_dir, grouping=grouping, p_cutoff=0.7, overwrite=True, sort_freqs_ascending=False
+    )
+    output_folder_structure = list_directory_tree(output_root_dir)
+    print("============================================================")
+    print(output_folder_structure)
+    print("============================================================")
+    assert output_folder_structure == ""
