f40d96a48f448e1e1afdd0aa91d2e1e6b0d39706
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-20 08:03:57 +0100

    Add helper function to list contents of a directory in a tree-like format

---
+++ chantstats/chantstats/utils.py
@@ -0,0 +1,24 @@
+import os
+import sh
+
+from .logging import logger
+
+__all__ = ["list_directory_tree"]
+
+
+def list_directory_tree(root_dir):
+    """
+    List contents of `root_dir` in a tree-like format.
+
+    Returns
+    -------
+    str
+    """
+    if not os.path.exists(root_dir):
+        logger.error(f"Cannot list tree under non-existent directory: '{root_dir}'")
+        return
+    cwd = os.getcwd()
+    os.chdir(root_dir)
+    res = sh.tree().stdout.decode()
+    os.chdir(cwd)
+    return res
