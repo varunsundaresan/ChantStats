e99ad05bb0be94bd762f3708177e9322cd2bcc76
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-20 09:37:49 +0100

    Add test for results export of ModalCategory (dendrogram plot and stacked bar charts)

--- chantstats/tests/conftest.py
+++ chantstats/tests/conftest.py
@@ -1,6 +1,12 @@
+import numpy as np
 import os
 import pytest
 from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+from itertools import count
+
+from .context import chantstats
+from chantstats import GroupingByModalCategory
+from chantstats.pitch_class_freqs import PCFreqs
 
 here = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,3 +30,41 @@ def diff_reporter():
     diff_reporter_factory = GenericDiffReporterFactory()
     diff_reporter_factory.load(os.path.join(here, "approvaltests_diff_reporters.json"))
     return diff_reporter_factory.get_first_working()
+
+
+pcs = ["A", "B", "C", "D", "E", "F", "G"]
+
+
+class DummyItem:
+    counter = count(start=1)
+
+    def __init__(self, *, final, ambitus):
+        self.index = next(self.counter)
+        self.descr = f"DummyItem #{self.index}"
+        self.final = final
+        self.ambitus = ambitus
+        self.pitch_classes = list(np.random.choice(pcs, size=100))
+        self.random_state = np.random.RandomState()
+        self.random_state.seed(self.index)
+        self.pc_freqs = PCFreqs(self.pitch_classes)
+
+
+def make_dummy_items(*, final, ambitus, num):
+    return [DummyItem(final=final, ambitus=ambitus) for _ in range(num)]
+
+
+dummy_items = sum(
+    [
+        make_dummy_items(final="C", ambitus="authentic", num=5),
+        make_dummy_items(final="D", ambitus="authentic", num=10),
+        make_dummy_items(final="D", ambitus="plagal", num=12),
+        make_dummy_items(final="G", ambitus="authentic", num=20),
+        make_dummy_items(final="G", ambitus="plagal", num=18),
+    ],
+    [],
+)
+
+
+@pytest.fixture(scope="session")
+def dummy_grouping():
+    return GroupingByModalCategory(dummy_items, group_by="final")
--- chantstats/tests/test_modal_category.py
+++ chantstats/tests/test_modal_category.py
@@ -1,5 +1,10 @@
+import textwrap
+from itertools import count
+
 from .context import chantstats
+from chantstats import FullAnalysisSpec
 from chantstats.modal_category import ModalCategory
+from chantstats.utils import list_directory_tree
 
 
 def test_modal_category_output_path_stub():
@@ -23,3 +28,31 @@ def test_modal_category_output_path_stub():
     mc = ModalCategory(items=[], modal_category_type="final_and_ambitus", key=("B", "plagal"))
     assert mc.output_path_stub == "plagal_modes/B_plagal"
     assert mc.descr == "B_plagal"
+
+
+# TODO: the setup for this test should be factored out!
+def test_export_results(dummy_grouping, tmpdir):
+    output_root_dir = str(tmpdir)
+    print(f"[DDD] dummy_grouping: {dummy_grouping}")
+    grp = dummy_grouping.groups["D"]
+    analysis_spec = FullAnalysisSpec(
+        repertoire_and_genre="plainchant_sequences", analysis="pc_freqs", unit="pcs", mode="authentic_modes"
+    )
+    grp.export_results(analysis_spec=analysis_spec, output_root_dir=output_root_dir, p_cutoff=0.7, overwrite=True)
+    dir_tree = list_directory_tree(output_root_dir)
+    expected_dir_tree = textwrap.dedent(
+        """\
+        .
+        └── chant
+            └── pc_freqs
+                └── sequences
+                    └── pcs
+                        └── authentic_modes
+                            └── D_authentic
+                                ├── dendrogram.png
+                                └── stacked_bar_chart.png
+
+        6 directories, 2 files
+        """
+    )
+    assert expected_dir_tree == dir_tree
