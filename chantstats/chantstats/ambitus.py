2af38da55f3068bd5bb4e915b4a8a4b52d2704fc
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-23 10:19:56 +0100

    Add alternative ambitus calculation function

--- chantstats/chantstats/ambitus.py
+++ chantstats/chantstats/ambitus.py
@@ -29,3 +29,33 @@ def get_ambitus(*, note_of_final, lowest_note):
             "We expect the lowest note to be less than an octave "
             "below the main final."
         )
+
+
+def calculate_ambitus(item):
+    """
+    Calculate the ambitus of a phrase, monomodal section, piece, etc.
+
+    Parameters
+    ----------
+    item
+        The item for which to calculate the ambitus. This can be of
+        any type, but it must have the attributes `lowest_note` and
+        `note_of_final` defined.
+    """
+    if item.note_of_final is None:
+        return AmbitusType.UNDEFINED
+
+    interval = Interval(item.note_of_final, item.lowest_note)
+    if 0 >= interval.semitones >= -4:
+        return AmbitusType.AUTHENTIC
+    elif -5 >= interval.semitones > -12:
+        return AmbitusType.PLAGAL
+    elif interval.semitones == -12:
+        # TODO: which ambitus should this have?
+        return AmbitusType.PLAGAL
+    else:
+        raise Exception(  # pragma: no cover
+            "Check the logic in the ambitus calculation! "
+            "We expect the lowest note to be less than an octave "
+            "below the main final."
+        )
