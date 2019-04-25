e77492a7d99ca4c6971ad49d4746870031ab6e50
Maximilian Albert <maximilian.albert@gmail.com>
2019-04-25 21:48:07 +0100

    Add alternative constructor

--- chantstats/chantstats/mode_degrees.py
+++ chantstats/chantstats/mode_degrees.py
@@ -6,17 +6,24 @@ major_c_scale = MajorScale(tonic="C")
 
 
 class ModeDegree:
-    def __init__(self, note, base_note):
-        self.note = note
-        self.base_note = base_note
-        self.diatonic_distance = (self.note.pitch.diatonicNoteNum - base_note.pitch.diatonicNoteNum) % 7 + 1
-        self.value = self.diatonic_distance
-        self.alter = self.note.pitch.alter
-        if self.alter not in [0, -1.0]:  # pragma: no cover
-            raise NotImplementedError(f"Unexpected alteration of note: '{self.note.nameWithOctave}'")
+    def __init__(self, *, value, alter):
+        assert value in [1, 2, 3, 4, 5, 6, 7]
+        assert alter in [0, -1.0]
 
+        self.value = value
+        self.alter = alter
         self.prefix = "flat-" if self.alter == -1.0 else ""
-        self.descr = f"{self.prefix}{self.diatonic_distance}"
+        self.descr = f"{self.prefix}{self.value}"
+
+    @classmethod
+    def from_note_pair(self, *, note, base_note):
+        diatonic_distance = (note.pitch.diatonicNoteNum - base_note.pitch.diatonicNoteNum) % 7 + 1
+        alter = note.pitch.alter
+
+        if alter not in [0, -1.0]:  # pragma: no cover
+            raise NotImplementedError(f"Unexpected alteration of note: '{note.nameWithOctave}'")
+
+        return ModeDegree(value=diatonic_distance, alter=alter)
 
     def __repr__(self):
         return f"<ModeDegree: {self.descr}>"
@@ -26,21 +33,19 @@ class ModeDegree:
 
     def __eq__(self, other):
         if isinstance(other, int):
-            return self.alter == 0 and self.diatonic_distance == other
+            return self.alter == 0 and self.value == other
         elif isinstance(other, str):
             return self.descr == other
         elif isinstance(other, ModeDegree):
-            return self.diatonic_distance == other.diatonic_distance and self.alter == other.alter
+            return self.value == other.value and self.alter == other.alter
         else:
             raise TypeError(f"Cannot compare ModeDegree to object of type {type(other)}")
 
     def __lt__(self, other):
-        return (self.diatonic_distance < other.diatonic_distance) or (
-            self.diatonic_distance == other.diatonic_distance and self.alter < other.alter
-        )
+        return (self.value < other.value) or (self.value == other.value and self.alter < other.alter)
 
     def __hash__(self):
-        return hash((self.diatonic_distance, self.alter))
+        return hash((self.value, self.alter))
 
 
 def calculate_mode_degrees(item):
@@ -59,5 +64,5 @@ def calculate_mode_degrees(item):
         The item for which to calculate mode degrees. This can be of any type
         but it must have the attributes `notes` and `note_of_final` defined.
     """
-    mode_degrees = [ModeDegree(note=n, base_note=item.note_of_final) for n in item.notes]
+    mode_degrees = [ModeDegree.from_note_pair(note=n, base_note=item.note_of_final) for n in item.notes]
     return mode_degrees
