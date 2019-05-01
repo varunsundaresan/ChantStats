190991e2b5d8758ad094ae59f7846fc3bc4ba659
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-01 16:36:19 +0100

    Add PCTendencies

---
+++ chantstats/chantstats/pitch_class_tendencies.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from .pitch_class_freqs import OCCURRING_PCS
+
+__all__ = ["PCTendencies"]
+
+
+class PCTendencies:
+    OCCURRING_PCS = OCCURRING_PCS
+
+    def __init__(self, df_pair_counts):
+        assert isinstance(df_pair_counts, pd.DataFrame)
+        assert set(df_pair_counts.index) == set(OCCURRING_PCS)
+        assert set(df_pair_counts.columns) == set(OCCURRING_PCS)
+        self.df_pair_counts = df_pair_counts
+
+        sums_per_col = self.df_pair_counts.sum(axis="index", skipna=False)
+        self.condprobs_v1 = 100 * self.df_pair_counts.div(sums_per_col, axis="columns")  # each column sums up to 100
+
+        # sums_per_row = self.df_pair_counts.sum(axis="columns", skipna=False)
+        # self.condprobs_v2 = 100 * self.df_pair_counts.div(sums_per_col, axis="columns")  # each column sums up to 100
+
+    @classmethod
+    def from_pitch_classes(cls, pcs):
+        assert set(pcs).issubset(OCCURRING_PCS)
+        s = pd.Series(pcs)
+        df = pd.DataFrame({"pc_A": s, "pc_B": s.shift(-1), "count": 1}).dropna()
+        df_pair_counts = (
+            df.pivot_table(index="pc_B", columns="pc_A", values="count", aggfunc=sum, fill_value=0)
+            .reindex(OCCURRING_PCS)
+            .fillna(0, downcast="infer")
+            .reindex(OCCURRING_PCS, axis=1)
+            .fillna(0, downcast="infer")
+        )
+        return cls(df_pair_counts)
+
+    def __add__(self, other):
+        cls = self.__class__
+        assert isinstance(other, cls)
+        return cls(self.df_pair_counts + other.df_pair_counts)
+
+    def get_condprobs_v1(self, given_pc):
+        return self.condprobs_v1[given_pc]
+
+    # @property
+    # def condprobs_v1(self):
+    #     df = self.df_pair_counts
+    #     sums_per_col = df.sum(axis="index", skipna=False)
+    #     condprobs_v1 = df.div(sums_per_col, axis="columns")  # each column sums up to 1.0
+    #     return condprobs_v1
+    #
+    # @property
+    # def condprobs_v2(self):
+    #     df = self.df_pair_counts
+    #     sums_per_row = df.sum(axis="columns", skipna=False)
+    #     condprobs_v2 = df.div(sums_per_row, axis="index")  # each row sums up to 1.0
+    #     return condprobs_v2
