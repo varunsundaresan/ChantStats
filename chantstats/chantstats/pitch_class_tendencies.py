0da2fc5bcfe714111fda49a2abb6900aabe83123
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-01 17:28:28 +0100

    Add PCTendencies.zeros

--- chantstats/chantstats/pitch_class_tendencies.py
+++ chantstats/chantstats/pitch_class_tendencies.py
@@ -4,13 +4,22 @@ from .pitch_class_freqs import OCCURRING_PCS
 __all__ = ["PCTendencies"]
 
 
+class classproperty(object):
+    # See https://stackoverflow.com/questions/5189699/how-to-make-a-class-property
+    def __init__(self, f):
+        self.f = f
+
+    def __get__(self, obj, owner):
+        return self.f(owner)
+
+
 class PCTendencies:
-    OCCURRING_PCS = OCCURRING_PCS
+    ALLOWED_VALUES = OCCURRING_PCS
 
     def __init__(self, df_pair_counts):
         assert isinstance(df_pair_counts, pd.DataFrame)
-        assert set(df_pair_counts.index) == set(OCCURRING_PCS)
-        assert set(df_pair_counts.columns) == set(OCCURRING_PCS)
+        assert set(df_pair_counts.index) == set(self.ALLOWED_VALUES)
+        assert set(df_pair_counts.columns) == set(self.ALLOWED_VALUES)
         self.df_pair_counts = df_pair_counts
 
         sums_per_col = self.df_pair_counts.sum(axis="index", skipna=False)
@@ -21,17 +30,23 @@ class PCTendencies:
 
     @classmethod
     def from_pitch_classes(cls, pcs):
-        assert set(pcs).issubset(OCCURRING_PCS)
+        assert set(pcs).issubset(cls.ALLOWED_VALUES)
         s = pd.Series(pcs)
         df = pd.DataFrame({"pc_A": s, "pc_B": s.shift(-1), "count": 1}).dropna()
         df_pair_counts = (
             df.pivot_table(index="pc_B", columns="pc_A", values="count", aggfunc=sum, fill_value=0)
-            .reindex(OCCURRING_PCS)
+            .reindex(cls.ALLOWED_VALUES)
             .fillna(0, downcast="infer")
-            .reindex(OCCURRING_PCS, axis=1)
+            .reindex(cls.ALLOWED_VALUES, axis=1)
             .fillna(0, downcast="infer")
         )
         return cls(df_pair_counts)
+
+    @classproperty
+    def zeros(cls):
+        # initialise the class with an empty list to obtain a "zero values" object
+        df_pair_counts = pd.DataFrame(0, index=OCCURRING_PCS, columns=OCCURRING_PCS)
+        return PCTendencies(df_pair_counts)
 
     def __add__(self, other):
         cls = self.__class__
