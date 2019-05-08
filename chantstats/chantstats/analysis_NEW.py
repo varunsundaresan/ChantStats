48e25203460050c523a73931368fd859e0add50f
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-08 21:24:43 +0100

    Load pieces from config and rep/genre

--- chantstats/chantstats/analysis_NEW.py
+++ chantstats/chantstats/analysis_NEW.py
@@ -3,7 +3,9 @@ from .plainchant_sequence_monomodal_sections import extract_monomodal_sections
 __all__ = ["prepare_analysis_inputs"]
 
 
-def prepare_analysis_inputs(pieces, repertoire_and_genre, mode, min_length_monomodal_sections=3):
+def prepare_analysis_inputs(repertoire_and_genre, mode, *, cfg, min_length_monomodal_sections=3, filename_pattern=None):
+    pieces = cfg.load_pieces(repertoire_and_genre, pattern=filename_pattern)
+
     if mode == "by_final":
         enforce_same_ambitus = False
     elif mode == "by_final_and_ambitus":
