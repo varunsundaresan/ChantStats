02980c1dc2d8e4593388295818034fd3306ba2d2
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-10 00:02:59 +0100

    Minor refactoring; add method to export AnalysisResultCollection into subfolder hierarchy

--- chantstats/chantstats/analysis_NEW.py
+++ chantstats/chantstats/analysis_NEW.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import shutil
 from collections import defaultdict
 from tqdm import tqdm
 
@@ -9,6 +10,7 @@ from .logging import logger
 from .modal_category import ModalCategoryType, GroupingByModalCategory
 from .unit import UnitType
 from .plainchant_sequence_monomodal_sections import extract_monomodal_sections
+from .results_export import get_color_palette_for_unit
 
 __all__ = ["prepare_analysis_inputs"]
 
@@ -37,7 +39,11 @@ def convert_to_nested_dict(d):
 class AnalysisResultCollection:
     def __init__(self):
         self._results = defaultdict(dict)
-        self._results_nested = recursive_defaultdict()  # this is redudant (could compute on the fly), but nice to have
+        self._results_json = defaultdict(dict)
+
+        # These are redundant (could compute from the above on the fly), but nice to have
+        self._results_nested = recursive_defaultdict()
+        self._results_nested_json = recursive_defaultdict()
 
     def __getitem__(self, path_stubs):
         return self._results[path_stubs]
@@ -56,15 +62,23 @@ class AnalysisResultCollection:
         )
 
     def insert_results(self, repertoire_and_genre, analysis, unit, modal_category, results_key, value):
+        if isinstance(value, list):
+            value_json = [x.to_json() for x in value]
+        else:
+            value_json = value.to_json()
+
         path_stubs = self._get_path_stubs(repertoire_and_genre, analysis, unit, modal_category)
+
         get_recursive_subdict(self._results_nested, path_stubs)[results_key] = value
+        get_recursive_subdict(self._results_nested_json, path_stubs)[results_key] = value_json
         self._results[path_stubs][results_key] = value
+        self._results_json[path_stubs][results_key] = value_json
 
     def to_dict(self):
-        return dict(self._results)  # convert from defaultdict to dict
+        return dict(self._results_json)  # convert from defaultdict to dict
 
     def to_nested_dict(self):
-        return convert_to_nested_dict(self._results_nested)
+        return convert_to_nested_dict(self._results_nested_json)
 
     def to_pickle(self, filename, overwrite=False):
         if os.path.exists(filename):
@@ -81,6 +95,32 @@ class AnalysisResultCollection:
     def from_pickle(cls, filename):
         with open(filename, "rb") as f:
             return pickle.load(f)
+
+    def export_plots(self, output_root_dir, overwrite=False):
+        if os.path.exists(output_root_dir):
+            if not overwrite:
+                logger.warn(f"Aborting because output root dir already exists (and overwrite=False): {output_root_dir}")
+                return
+            else:
+                logger.warn(f"Removing existing output root dir: {output_root_dir}")
+                shutil.rmtree(output_root_dir)
+
+        for path_stubs in self.to_dict().keys():
+            output_dir = os.path.join(output_root_dir, *path_stubs)
+            logger.debug(f"Exporting results to folder: {output_dir}")
+
+            dendrogram = self._results[path_stubs]["dendrogram"]
+
+            # Export dendrogram
+            os.makedirs(output_dir, exist_ok=True)
+            fig = dendrogram.plot_dendrogram()
+            fig.savefig(os.path.join(output_dir, "dendrogram.png"))
+
+            # Export stacked bar chart
+            unit = path_stubs[3]
+            color_palette = get_color_palette_for_unit(unit)
+            fig = dendrogram.plot_stacked_bar_charts(color_palette=color_palette)
+            fig.savefig(os.path.join(output_dir, "stacked_bar_chart.png"))
 
 
 def prepare_analysis_inputs(repertoire_and_genre, mode, *, cfg, min_length_monomodal_sections=3, filename_pattern=None):
@@ -102,7 +142,11 @@ def accumulate_results(
 ):
     results = existing_results or AnalysisResultCollection()
     analysis_inputs = prepare_analysis_inputs(
-        repertoire_and_genre, mode, cfg=cfg, min_length_monomodal_sections=3, filename_pattern=None
+        repertoire_and_genre,
+        mode,
+        cfg=cfg,
+        min_length_monomodal_sections=min_length_monomodal_sections,
+        filename_pattern=None,
     )
 
     grouping = GroupingByModalCategory(analysis_inputs, group_by=mode)
@@ -112,14 +156,9 @@ def accumulate_results(
         df = modal_category.make_results_dataframe(analysis_func=AnalysisFuncPCFreqs("rel_freqs"), unit=unit)
         dendrogram = Dendrogram(df, p_threshold=p_cutoff)
 
-        results.insert_results(repertoire_and_genre, analysis, unit, modal_category, "dendrogram", dendrogram.to_json())
+        results.insert_results(repertoire_and_genre, analysis, unit, modal_category, "dendrogram", dendrogram)
         results.insert_results(
-            repertoire_and_genre,
-            analysis,
-            unit,
-            modal_category,
-            "clusters",
-            [c.to_json() for c in dendrogram.nodes_below_cutoff],
+            repertoire_and_genre, analysis, unit, modal_category, "clusters", [c for c in dendrogram.nodes_below_cutoff]
         )
 
     return results
