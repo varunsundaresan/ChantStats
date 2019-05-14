239cdcc4b800975284098c6d8af3bd98f8f211ee
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-14 18:43:57 +0100

    Add toplevel imports

--- chantstats/chantstats/__init__.py
+++ chantstats/chantstats/__init__.py
@@ -1,8 +1,9 @@
 from .analysis_spec import FullAnalysisSpec
+from .config import ChantStatsConfig
 from .dendrogram import plot_dendrogram
 from .logging import logger
 from .modal_category import GroupingByModalCategory
-from .plainchant_sequence_piece import load_plainchant_sequence_pieces
+from .plainchant_sequence_piece import load_plainchant_sequence_pieces, load_pieces
 from .plainchant_sequence_monomodal_sections import MonomodalSection, extract_monomodal_sections
 from .phrase_collection import PhraseCollection
 from .results_export import export_dendrogram_and_stacked_bar_chart
@@ -11,6 +12,8 @@ __all__ = [
     "FullAnalysisSpec",
     "PhraseCollection",
     "GroupingByModalCategory",
+    "ChantStatsConfig",
+    "load_pieces",
     "load_plainchant_sequence_pieces",
     "logger",
     "chantstats_logger",
