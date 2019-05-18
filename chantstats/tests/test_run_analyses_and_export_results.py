7342b6cd9f2def9e9673fe3014ae89dbb6e85e83
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-19 00:31:52 +0100

    Add test for tendencies (only for unit='pcs' for now)

--- chantstats/tests/test_run_analyses_and_export_results.py
+++ chantstats/tests/test_run_analyses_and_export_results.py
@@ -31,3 +31,27 @@ def test_run_pc_freqs_analysis_and_export_results(tmpdir, diff_reporter):
 
     exported_files = list_directory_tree(output_root_dir)
     verify(exported_files, diff_reporter)
+
+
+@pytest.mark.slow
+def test_run_pc_freqs_analysis_and_export_results(tmpdir, diff_reporter):
+    output_root_dir = os.getenv("CHANTSTATS_OUTPUT_ROOT_DIR", str(tmpdir))
+    logger.info(f"Using output root dir: '{output_root_dir}'")
+
+    cfg = ChantStatsConfig(musicxml_paths={"plainchant_sequences": chants_dir})
+    repertoire_and_genre = "plainchant_sequences"
+    analysis_name = "pc_tendencies"
+    min_length_monomodal_sections = 3
+
+    results = calculate_results(
+        repertoire_and_genre,
+        analysis_name,
+        cfg,
+        min_length_monomodal_sections=min_length_monomodal_sections,
+        units=["pcs"],
+    )
+    export_results(results, output_root_dir, p_cutoff=0.7)
+    export_results(results, output_root_dir, p_cutoff=0.15)
+
+    exported_files = list_directory_tree(output_root_dir)
+    verify(exported_files, diff_reporter)
