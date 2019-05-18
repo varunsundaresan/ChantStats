af2bf20f5554fdffc7f9cff44e331f90a96ff4a3
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-18 21:23:03 +0100

    Fix tests

--- chantstats/tests/test_modal_category.py
+++ chantstats/tests/test_modal_category.py
@@ -10,24 +10,30 @@ from chantstats.utils import list_directory_tree
 
 def test_modal_category_output_path_stub():
     mc = ModalCategory(items=[], modal_category_type="final", key="D")
-    assert mc.output_path_stub == "by_final/D_final"
+    assert mc.output_path_stub_1 == "by_final"
+    assert mc.output_path_stub_2 == "D_final"
     assert mc.descr == "D-final"
     mc = ModalCategory(items=[], modal_category_type="final", key="G")
-    assert mc.output_path_stub == "by_final/G_final"
+    assert mc.output_path_stub_1 == "by_final"
+    assert mc.output_path_stub_2 == "G_final"
     assert mc.descr == "G-final"
 
     mc = ModalCategory(items=[], modal_category_type="final_and_ambitus", key=("C", "authentic"))
-    assert mc.output_path_stub == "authentic_modes/C_authentic"
+    assert mc.output_path_stub_1 == "authentic_modes"
+    assert mc.output_path_stub_2 == "C_authentic"
     assert mc.descr == "C-authentic"
     mc = ModalCategory(items=[], modal_category_type="final_and_ambitus", key=("F", "authentic"))
-    assert mc.output_path_stub == "authentic_modes/F_authentic"
+    assert mc.output_path_stub_1 == "authentic_modes"
+    assert mc.output_path_stub_2 == "F_authentic"
     assert mc.descr == "F-authentic"
 
     mc = ModalCategory(items=[], modal_category_type="final_and_ambitus", key=("A", "plagal"))
-    assert mc.output_path_stub == "plagal_modes/A_plagal"
+    assert mc.output_path_stub_1 == "plagal_modes"
+    assert mc.output_path_stub_2 == "A_plagal"
     assert mc.descr == "A-plagal"
     mc = ModalCategory(items=[], modal_category_type="final_and_ambitus", key=("B", "plagal"))
-    assert mc.output_path_stub == "plagal_modes/B_plagal"
+    assert mc.output_path_stub_1 == "plagal_modes"
+    assert mc.output_path_stub_2 == "B_plagal"
     assert mc.descr == "B-plagal"
 
 
