916eea4f96fb70439197550d4c797db71e0ccce6
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-19 23:46:04 +0100

    Fix import of sample pieces

--- chantstats/tests/sample_pieces.py
+++ chantstats/tests/sample_pieces.py
@@ -1,5 +1,5 @@
 import os
-from chantstats import load_plainchant_sequence_pieces
+from chantstats import load_pieces, ChantStatsConfig
 from chantstats.plainchant_sequence_piece import PlainchantSequencePiece
 
 try:
@@ -18,11 +18,14 @@ except KeyError:
 #  - piece6 has heavy polymodal frame 1 (no amen formula)
 #  - piece7 has monomodal frame (with amen formula)
 #  - piece8 has monomodal frame (without amen formula)
-pieces_1_thru_8 = load_plainchant_sequence_pieces(chants_dir, pattern="BN_lat_1112_Sequence_0[1-8]*.xml")
+cfg = ChantStatsConfig.from_env()
+#pieces_1_thru_8 = load_plainchant_sequence_pieces(chants_dir, pattern="BN_lat_1112_Sequence_0[1-8]*.xml")
+pieces_1_thru_8 = load_pieces("plainchant_sequences", cfg, filename_pattern="BN_lat_1112_Sequence_0[1-8]*.xml").pieces
 piece1, piece2, piece3, piece4, piece5, piece6, piece7, piece8 = pieces_1_thru_8
 
+sequences_dir = os.path.join(chants_dir, "BN_lat_1112_Sequences", "musicxml")
 prefix="BN_lat_1112_Sequence_"
-piece12 = PlainchantSequencePiece(os.path.join(chants_dir, prefix+"12_Fulgens_preclara.xml"))        # heavy polymodal frame 1 (no amen formula)
-piece14 = PlainchantSequencePiece(os.path.join(chants_dir, prefix+"14_Zima_uetus.xml"))              # light polymodal frame 1 (with amen formula)
-piece32 = PlainchantSequencePiece(os.path.join(chants_dir, prefix+"32_Magnus_deus.xml"))             # heavy polymodal frame 2 (with amen formula)
-piece45 = PlainchantSequencePiece(os.path.join(chants_dir, prefix+"45_Aue_maria_gratia_plena.xml"))  # light polymodal frame 2 (with amen formula)
+piece12 = PlainchantSequencePiece(os.path.join(sequences_dir, prefix+"12_Fulgens_preclara.xml"))        # heavy polymodal frame 1 (no amen formula)
+piece14 = PlainchantSequencePiece(os.path.join(sequences_dir, prefix+"14_Zima_uetus.xml"))              # light polymodal frame 1 (with amen formula)
+piece32 = PlainchantSequencePiece(os.path.join(sequences_dir, prefix+"32_Magnus_deus.xml"))             # heavy polymodal frame 2 (with amen formula)
+piece45 = PlainchantSequencePiece(os.path.join(sequences_dir, prefix+"45_Aue_maria_gratia_plena.xml"))  # light polymodal frame 2 (with amen formula)
