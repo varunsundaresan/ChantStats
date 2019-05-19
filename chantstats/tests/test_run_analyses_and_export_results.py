00b0e4d24dcac0f1e9297c788694bcdbdf92be1f
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-19 23:35:06 +0100

    Simplify test setup

--- chantstats/tests/test_run_analyses_and_export_results.py
+++ chantstats/tests/test_run_analyses_and_export_results.py
@@ -1,16 +1,10 @@
 import os
 import pytest
-import sh
 from approvaltests.approvals import verify
 
 from .context import chantstats
 from chantstats import ChantStatsConfig, calculate_results, export_results, logger
 from chantstats.utils import list_directory_tree
-
-try:
-    chants_dir = os.environ["CHANTS_DIR"]
-except KeyError:
-    raise RuntimeError("The environment variable CHANTS_DIR must be defined to run the tests.")
 
 
 @pytest.mark.slow
@@ -18,7 +12,7 @@ def test_run_analyses_and_export_results(tmpdir, diff_reporter):
     output_root_dir = os.getenv("CHANTSTATS_OUTPUT_ROOT_DIR", str(tmpdir))
     logger.info(f"Using output root dir: '{output_root_dir}'")
 
-    cfg = ChantStatsConfig(musicxml_paths={"plainchant_sequences": chants_dir})
+    cfg = ChantStatsConfig.from_env()
     repertoire_and_genre = "plainchant_sequences"
     min_length_monomodal_sections = 3
 
