37fd50d01a8e61dc47dd3fac630e8e6b1cbb87fa
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-22 00:23:46 +0100

    Add base freqs and PC freqs

--- chantstats/chantstats/v2/__init__.py
+++ chantstats/chantstats/v2/__init__.py
@@ -1,2 +1,3 @@
 from .config import ChantStatsConfig
 from .plainchant_sequence_piece import load_pieces
+from .modal_category import GroupingByModalCategory
---
+++ chantstats/chantstats/v2/freqs.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import textwrap
+from collections import Counter
+from .pitch_class import PC
+
+
+class BaseFreqsMeta(type):
+
+    # Note: the only purpose of this metaclass is to ensure that the `BaseFreqs`
+    # class has a class attribute called `zero_freqs` which can be used as the
+    # "zero frequencies value" when calculating sums of frequencies using sum().
+
+    @property
+    def zero_freqs(cls):
+        # initialise the class with an empty list to obtain a "zero frequencies" object
+        return cls([])
+
+
+class BaseFreqs(metaclass=BaseFreqsMeta):
+    """
+    Base class representing absolute/relative frequencies of some entity
+    (for example, of pitch classes). This base class provides all the
+    machinery; derived classes only need to set ALLOWED_VALUES.
+    """
+
+    ALLOWED_VALUES = []  # derived classes need to set this to a list of allowed values
+
+    def __init__(self, list_or_freqs):
+        if isinstance(list_or_freqs, pd.Series):
+            freqs = list_or_freqs
+            assert list(freqs.index == self.ALLOWED_VALUES)
+            self.abs_freqs = freqs
+        elif isinstance(list_or_freqs, list):
+            if not set(list_or_freqs).issubset(self.ALLOWED_VALUES):
+                raise ValueError(
+                    f"Unexpected values: {set(list_or_freqs)}. Must be a subset of allowed values: {self.ALLOWED_VALUES}"
+                )
+            self.abs_freqs = pd.Series(Counter(list_or_freqs), index=self.ALLOWED_VALUES).fillna(0, downcast="infer")
+        else:  # pragma: no cover
+            raise ValueError(f"Cannot instantiate {self.__class__.__name__} from object: {list_or_freqs}")
+
+    def __add__(self, other):
+        assert isinstance(other, self.__class__)
+        return self.__class__(self.abs_freqs + other.abs_freqs)
+
+    def __repr__(self):
+        freqs_indented = textwrap.indent(str(self.abs_freqs), prefix="   ")
+        return f"<{self.__class__.__name__}:\n\n{freqs_indented}\n>"
+
+    @property
+    def rel_freqs(self):
+        return (self.abs_freqs / self.abs_freqs.sum()) * 100
+
+
+class PCFreqs(BaseFreqs):
+    ALLOWED_VALUES = list(PC)
