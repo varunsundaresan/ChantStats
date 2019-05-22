291842590855c9e102d1acc68940f952d0e831c6
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-22 23:20:10 +0100

    Add test for results export

--- chantstats/chantstats/v2/utils.py
+++ chantstats/chantstats/v2/utils.py
@@ -1,11 +1,32 @@
 import numpy as np
+import os
+import sh
 from enum import Enum
+from .logging import logger
 
 __all__ = ["EnumWithDescription"]
 
 
 def is_close_to_zero_or_100(x):
     return np.isclose(x, 0.0) or np.isclose(x, 100.0)
+
+
+def list_directory_tree(root_dir):
+    """
+    List contents of `root_dir` in a tree-like format.
+
+    Returns
+    -------
+    str
+    """
+    if not os.path.exists(root_dir):
+        logger.error(f"Cannot list tree under non-existent directory: '{root_dir}'")
+        return
+    cwd = os.getcwd()
+    os.chdir(root_dir)
+    res = sh.tree().stdout.decode("utf-8")
+    os.chdir(cwd)
+    return res
 
 
 class EnumWithDescription(str, Enum):
---
+++ chantstats/tests/v2/test_run_analyses_and_export_results.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+from approvaltests.approvals import verify
+
+from .context import chantstats
+from chantstats.v2 import ChantStatsConfig, calculate_results, export_results, logger
+from chantstats.v2.utils import list_directory_tree
+
+
+@pytest.mark.slow
+def test_run_analyses_and_export_results(tmpdir, diff_reporter):
+    output_root_dir = os.getenv("CHANTSTATS_OUTPUT_ROOT_DIR", str(tmpdir))
+    logger.info(f"Using output root dir: '{output_root_dir}'")
+
+    cfg = ChantStatsConfig.from_env()
+    repertoire_and_genre = "plainchant_sequences"
+    min_length_monomodal_sections = 3
+
+    # Calculate results for PC frequencies
+    results_pc_freqs = calculate_results(
+        repertoire_and_genre,
+        analysis="pc_freqs",
+        cfg=cfg,
+        min_length_monomodal_sections=min_length_monomodal_sections,
+        units=["pcs"],  # FIXME: support both units
+    )
+
+    # # Calculate results for PC tendencies
+    # results_pc_tendencies = calculate_results(
+    #     repertoire_and_genre,
+    #     analysis="pc_tendencies",
+    #     units=["pcs"],
+    #     cfg=cfg,
+    #     min_length_monomodal_sections=min_length_monomodal_sections,
+    # )
+
+    # Export all results
+    for p_cutoff in [0.7, 0.15]:
+        export_results(results_pc_freqs, output_root_dir, p_cutoff=p_cutoff)
+        # export_results(results_pc_tendencies, output_root_dir, p_cutoff=p_cutoff)
+
+    exported_files = list_directory_tree(output_root_dir)
+    verify(exported_files, diff_reporter)
