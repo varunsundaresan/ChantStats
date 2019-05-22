c52429f755b15aa6afc34f88f59859bbe4f5dc04
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-22 22:38:50 +0100

    Add helper function

--- chantstats/chantstats/v2/utils.py
+++ chantstats/chantstats/v2/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from enum import Enum
 
 __all__ = ["EnumWithDescription"]
@@ -19,3 +20,7 @@ class EnumWithDescription(str, Enum):
             Long form description of this enum suitable for use in plot titles etc.
         """
         return self._description
+
+
+def is_close_to_zero_or_100(x):
+    return np.isclose(x, 0.0) or np.isclose(x, 100.0)
