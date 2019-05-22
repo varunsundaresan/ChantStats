6da2e664cdbeb19321a925dc01622fadf0b4dd82
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-22 17:16:28 +0100

    Test pitch class descr attribute

--- chantstats/tests/v2/test_pitch_class.py
+++ chantstats/tests/v2/test_pitch_class.py
@@ -14,3 +14,10 @@ def test_pitch_class_from_note():
     assert "A" == PC.A == PC.from_note(Note("A4"))
     assert "B-" == PC.B_FLAT == PC.from_note(Note("B-3"))
     assert "G" == PC.G == PC.from_note(Note("G2"))
+
+
+def test_pitch_class_description():
+    assert "A" == PC.A.descr
+    assert "B-" == PC.B_FLAT.descr
+    assert "F" == PC.F.descr
+    assert "G" == PC.G.descr
