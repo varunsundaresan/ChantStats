8f4485fc86c9b59f96a3cb7b8d246c3640408218
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-25 23:52:27 +0100

    Nicer legend labels for mode degrees

--- chantstats/chantstats/v2/mode_degree.py
+++ chantstats/chantstats/v2/mode_degree.py
@@ -10,7 +10,11 @@ class ModeDegree:
         self.alter = alter
         self.prefix = "flat-" if self.alter == -1.0 else ""
         self.descr = f"{self.prefix}{self.value}"
-        self.str_value = f"{self.prefix}{self.value}\u0302"  # same as 'descr', but with a hat symbol over the number
+        self.str_prefix = "♭" if self.alter == -1.0 else ""
+        # self.str_value = f"{self.str_prefix}{self.value}\u0302"  # same as 'descr', but with a hat symbol over the number
+        self.str_value = (
+            f"{self.str_prefix}$\widehat{self.value}$"
+        )  # same as 'descr', but with a hat symbol over the number
 
     @classmethod
     def from_note_pair(self, *, note, base_note):
