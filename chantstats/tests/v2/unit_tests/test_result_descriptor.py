2a9f9d24d4ecd527ec820ea5d4df7b5c920024bf
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-26 15:44:15 +0100

    Tweak output dirname/filename again

--- chantstats/tests/v2/unit_tests/test_result_descriptor.py
+++ chantstats/tests/v2/unit_tests/test_result_descriptor.py
@@ -7,33 +7,26 @@ def test_result_descriptor():
     output_root_dir = "/tmp/foo/"
     modal_category = ModalCategory(items=None, modal_category_type="final", key="G")
     rd = ResultDescriptor("plainchant_sequences", "pc_freqs", "pcs", modal_category)
-    assert rd.output_dirname == "chant/pc_freqs/sequences/pcs/by_final/final_G"
+    assert rd.output_dirname == "chant/pc_freqs/sequences/pcs"
+    assert rd.get_output_dir(output_root_dir) == "/tmp/foo/chant/pc_freqs/sequences/pcs"
+    assert rd.get_output_dir(output_root_dir) == "/tmp/foo/chant/pc_freqs/sequences/pcs"
     assert (
-        rd.get_output_dir(output_root_dir, p_cutoff=0.3)
-        == "/tmp/foo/p_cutoff_0.30/chant/pc_freqs/sequences/pcs/by_final/final_G"
+        rd.get_full_output_path(output_root_dir, "dendrogram.png")
+        == "/tmp/foo/chant/pc_freqs/sequences/pcs/final_G|dendrogram.png"
     )
     assert (
-        rd.get_output_dir(output_root_dir, p_cutoff=0.4)
-        == "/tmp/foo/p_cutoff_0.40/chant/pc_freqs/sequences/pcs/by_final/final_G"
-    )
-    assert (
-        rd.get_full_output_path(output_root_dir, "dendrogram.png", p_cutoff=0.2)
-        == "/tmp/foo/p_cutoff_0.20/chant/pc_freqs/sequences/pcs/by_final/final_G/dendrogram.png"
-    )
-    assert (
-        rd.get_full_output_path(output_root_dir, "quux.png", p_cutoff=0.6)
-        == "/tmp/foo/p_cutoff_0.60/chant/pc_freqs/sequences/pcs/by_final/final_G/quux.png"
+        rd.get_full_output_path(output_root_dir, "quux.png") == "/tmp/foo/chant/pc_freqs/sequences/pcs/final_G|quux.png"
     )
 
     output_root_dir = "/quux/bar"
     modal_category = ModalCategory(items=None, modal_category_type="final_and_ambitus", key=("C", "plagal"))
     rd = ResultDescriptor("plainchant_sequences", "tendency", "mode_degrees", modal_category)
-    assert rd.output_dirname == "chant/tendency/sequences/mode_degrees/plagal_modes/plagal_C"
+    assert rd.output_dirname == "chant/tendency/sequences/mode_degrees"
     assert (
-        rd.get_full_output_path(output_root_dir, "dendrogram.png", p_cutoff=0.15)
-        == "/quux/bar/p_cutoff_0.15/chant/tendency/sequences/mode_degrees/plagal_modes/plagal_C/dendrogram.png"
+        rd.get_full_output_path(output_root_dir, "dendrogram.png")
+        == "/quux/bar/chant/tendency/sequences/mode_degrees/plagal_C|dendrogram.png"
     )
     assert (
-        rd.get_full_output_path(output_root_dir, "foobar.png", p_cutoff=0.04)
-        == "/quux/bar/p_cutoff_0.04/chant/tendency/sequences/mode_degrees/plagal_modes/plagal_C/foobar.png"
+        rd.get_full_output_path(output_root_dir, "foobar.png")
+        == "/quux/bar/chant/tendency/sequences/mode_degrees/plagal_C|foobar.png"
     )
