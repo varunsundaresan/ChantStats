a1bf70d9b9f111d0dfeb48f1b45fe7673abd7ca4
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-27 22:17:29 +0100

    Add LeapL5inMD

--- chantstats/chantstats/v2/leap.py
+++ chantstats/chantstats/v2/leap.py
@@ -1,12 +1,16 @@
 from .note_pair import NotePair
+from .mode_degree import ModeDegree
+from .pitch_class import PC
 
 __all__ = ["LeapL5"]
 
 
 class LeapL5:
     def __init__(self, *, bottom_pc, top_pc):
-        self.bottom_pc = bottom_pc
-        self.top_pc = top_pc
+        # assert isinstance(bottom_pc, PC)
+        # assert isinstance(top_pc, PC)
+        self.bottom_pc = PC(bottom_pc)
+        self.top_pc = PC(top_pc)
 
     def __repr__(self):
         return f"pcs{self.bottom_pc}^{self.top_pc}_L5"
@@ -17,7 +21,7 @@ class LeapL5:
     def __lt__(self, other):
         # Note: this ordering relation doesn't really have much meaning, but we
         # need to implement __lt__ in order to be able to sort lists containing
-        # LeapL5 object.s
+        # LeapL5 objects.
         return self.bottom_pc < other.bottom_pc
 
     def __hash__(self):
@@ -35,13 +39,64 @@ class LeapL5:
             raise ValueError(f"Not a leap L5: {note_pair}")
         return cls(bottom_pc=note_pair.bottom_pc, top_pc=note_pair.top_pc)
 
+    def in_mode_degrees(self, *, base_pc):
+        bottom_md = ModeDegree.from_pc_pair(pc=self.bottom_pc, base_pc=base_pc)
+        top_md = ModeDegree.from_pc_pair(pc=self.top_pc, base_pc=base_pc)
+        return LeapL5inMD(bottom_md=bottom_md, top_md=top_md)
+
 
 LeapL5.allowed_values = [
-    LeapL5(bottom_pc="D", top_pc="A"),
-    LeapL5(bottom_pc="E", top_pc="B"),
-    LeapL5(bottom_pc="F", top_pc="C"),
-    LeapL5(bottom_pc="G", top_pc="D"),
-    LeapL5(bottom_pc="A", top_pc="E"),
-    LeapL5(bottom_pc="B-", top_pc="F"),
-    LeapL5(bottom_pc="C", top_pc="G"),
+    LeapL5(bottom_pc=PC("D"), top_pc=PC("A")),
+    LeapL5(bottom_pc=PC("E"), top_pc=PC("B")),
+    LeapL5(bottom_pc=PC("F"), top_pc=PC("C")),
+    LeapL5(bottom_pc=PC("G"), top_pc=PC("D")),
+    LeapL5(bottom_pc=PC("A"), top_pc=PC("E")),
+    LeapL5(bottom_pc=PC("B-"), top_pc=PC("F")),
+    LeapL5(bottom_pc=PC("C"), top_pc=PC("G")),
+]
+
+
+class LeapL5inMD:
+    def __init__(self, *, bottom_md, top_md):
+        assert isinstance(bottom_md, ModeDegree)
+        assert isinstance(top_md, ModeDegree)
+        self.bottom_md = bottom_md
+        self.top_md = top_md
+
+    def __repr__(self):
+        return f"mds{self.bottom_md}^{self.top_md}_L5"
+
+    def __eq__(self, other):
+        return self.bottom_md == other.bottom_md and self.top_md == other.top_md
+
+    def __lt__(self, other):
+        # Note: this ordering relation doesn't really have much meaning, but we
+        # need to implement __lt__ in order to be able to sort lists containing
+        # LeapL5 objects.
+        return self.bottom_md < other.bottom_md
+
+    def __hash__(self):
+        return hash((self.bottom_md, self.top_md))
+
+    #     @property
+    #     def str_value(self):
+    #         return rf"pcs$\stackrel{{{self.top_pc}}}{{{self.bottom_pc}}}$_L5"
+    #         #return f"pcs${{\mathrm{{{self.top_pc}}}}}^{{\mathrm{{{self.bottom_pc}}}}}$_L5"
+    #
+    # @classmethod
+    # def from_note_pair(cls, note_pair):
+    #     assert isinstance(note_pair, NotePair)
+    #     if not note_pair.semitones == 7:
+    #         raise ValueError(f"Not a leap L5: {note_pair}")
+    #     return cls(bottom_pc=note_pair.bottom_pc, top_pc=note_pair.top_pc)
+
+
+LeapL5inMD.allowed_values = [
+    LeapL5inMD(bottom_md=ModeDegree(value=1), top_md=ModeDegree(value=5)),
+    LeapL5inMD(bottom_md=ModeDegree(value=2), top_md=ModeDegree(value=6)),
+    LeapL5inMD(bottom_md=ModeDegree(value=3), top_md=ModeDegree(value=7)),
+    LeapL5inMD(bottom_md=ModeDegree(value=4), top_md=ModeDegree(value=1)),
+    LeapL5inMD(bottom_md=ModeDegree(value=5), top_md=ModeDegree(value=2)),
+    LeapL5inMD(bottom_md=ModeDegree(value=6), top_md=ModeDegree(value=3)),
+    LeapL5inMD(bottom_md=ModeDegree(value=7), top_md=ModeDegree(value=4)),
 ]
