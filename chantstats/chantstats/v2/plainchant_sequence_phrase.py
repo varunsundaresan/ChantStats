b95ef36cd0e0d330e562875962f3e140a7215710
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-29 00:03:56 +0100

    Add explicit MelodicOutline class

--- chantstats/chantstats/v2/plainchant_sequence_phrase.py
+++ chantstats/chantstats/v2/plainchant_sequence_phrase.py
@@ -20,6 +20,19 @@ def has_framing_interval(notes, interval_name):
     return Interval(notes[0], notes[-1]).name == interval_name
 
 
+class MelodicOutline:
+    def __init__(self, notes):
+        self.notes = notes
+        self.framing_note_pair = NotePair(self.notes[0], self.notes[-1])
+        self.framing_interval = self.framing_note_pair.interval
+        assert self.framing_interval.name in ["P4", "P5"]
+        self.bottom_pc = self.framing_note_pair.bottom_pc
+        self.top_pc = self.framing_note_pair.top_pc
+
+    def __repr__(self):
+        return f"<MO: pcs{self.bottom_pc}^{self.top_pc}_M{self.framing_interval.name[-1]}, {[n.nameWithOctave for n in self.notes]}>"
+
+
 class PlainchantSequencePhrase:
     """
     Represents a phrase in a plainchant sequence piece.
@@ -63,7 +76,7 @@ class PlainchantSequencePhrase:
 
     def get_melodic_outlines(self, interval, *, allow_thirds=False):
         return [
-            candidate
+            MelodicOutline(candidate)
             for candidate in self._melodic_outline_candidates
             if has_framing_interval(candidate, interval) and check_step_size(candidate, allow_thirds)
         ]
