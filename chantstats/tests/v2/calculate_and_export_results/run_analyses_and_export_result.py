0d7349185b7b08436d80e55a080debc25f46361c
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-31 00:42:28 +0100

    Split runs for different reps/genres into parallel jobs

---
+++ chantstats/tests/v2/calculate_and_export_results/run_analyses_and_export_result.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from chantstats.v2 import ChantStatsConfig, calculate_results, export_results, logger, load_pieces
+from chantstats.v2.repertoire_and_genre import RepertoireAndGenreType
+
+
+def run_analyses_and_export_results(rep_and_genre, *, output_root_dir):
+    logger.info(f"Using output root dir: '{output_root_dir}'")
+
+    cfg = ChantStatsConfig.from_env()
+    p_cutoff = 0.4
+    sampling_fraction = 0.7
+    sampling_seed = 99999
+    min_num_phrases_per_monomodal_section = 3
+    min_num_notes_per_monomodal_section = 80
+
+    pieces = load_pieces(rep_and_genre, cfg)
+
+    # for analysis in ["pc_freqs", "tendency", "L_and_M__L5_u_M5", "L_and_M__L4_u_M4"]:
+    for analysis in ["L_and_M__L5_u_M5", "L_and_M__L4_u_M4"]:
+        logger.info(f"Calculating results for analysis '{analysis}'")
+        results = calculate_results(
+            pieces=pieces,
+            analysis=analysis,
+            sampling_fraction=sampling_fraction,
+            sampling_seed=sampling_seed,
+            min_num_phrases_per_monomodal_section=min_num_phrases_per_monomodal_section,
+            min_num_notes_per_monomodal_section=min_num_notes_per_monomodal_section,
+        )
+
+        logger.info(f"Exporting results for analysis '{analysis}'")
+        export_results(results, output_root_dir, p_cutoff=p_cutoff)
+
+
+if __name__ == "__main__":
+    output_root_dir = os.environ["CHANTSTATS_OUTPUT_ROOT_DIR"]
+    rep_and_genre = RepertoireAndGenreType(sys.argv[1])
+    run_analyses_and_export_results(rep_and_genre, output_root_dir=output_root_dir)
--- chantstats/tests/v2/calculate_and_export_results/test_run_analyses_and_export_results.py
+++ chantstats/tests/v2/calculate_and_export_results/test_run_analyses_and_export_results.py
@@ -5,6 +5,7 @@ from approvaltests.approvals import verify
 from .context import chantstats
 from chantstats.v2 import ChantStatsConfig, calculate_results, export_results, logger, load_pieces
 from chantstats.v2.utils import list_directory_tree
+from .run_analyses_and_export_result import run_analyses_and_export_results
 
 
 @pytest.mark.slow
@@ -73,29 +74,5 @@ def test_folder_structure_for_exported_results(tmpdir, diff_reporter):
 @pytest.mark.slow
 def test_run_analyses_and_export_results(tmpdir):
     output_root_dir = os.getenv("CHANTSTATS_OUTPUT_ROOT_DIR", str(tmpdir))
-    logger.info(f"Using output root dir: '{output_root_dir}'")
-
-    cfg = ChantStatsConfig.from_env()
-    repertoires_and_genres = ["plainchant_sequences", "responsorial_chants"]
-    p_cutoff = 0.4
-    sampling_fraction = 0.7
-    sampling_seed = 99999
-    min_num_phrases_per_monomodal_section = 3
-    min_num_notes_per_monomodal_section = 80
-
-    for rep_and_genre in repertoires_and_genres:
-        pieces = load_pieces(rep_and_genre, cfg)
-
-        for analysis in ["pc_freqs", "tendency", "L_and_M__L5_u_M5", "L_and_M__L4_u_M4"]:
-            logger.info(f"Calculating results for analysis '{analysis}'")
-            results = calculate_results(
-                pieces=pieces,
-                analysis=analysis,
-                sampling_fraction=sampling_fraction,
-                sampling_seed=sampling_seed,
-                min_num_phrases_per_monomodal_section=min_num_phrases_per_monomodal_section,
-                min_num_notes_per_monomodal_section=min_num_notes_per_monomodal_section,
-            )
-
-            logger.info(f"Exporting results for analysis '{analysis}'")
-            export_results(results, output_root_dir, p_cutoff=p_cutoff)
+    run_analyses_and_export_results("plainchant_sequences", output_root_dir=output_root_dir)
+    run_analyses_and_export_results("responsorial_chants", output_root_dir=output_root_dir)
