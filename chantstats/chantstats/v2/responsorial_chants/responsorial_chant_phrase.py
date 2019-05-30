9350aab26c73ea2e1d45932126fa06b6494fd789
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-30 07:01:26 +0100

    Check for double or final barline

--- chantstats/chantstats/v2/responsorial_chants/responsorial_chant_phrase.py
+++ chantstats/chantstats/v2/responsorial_chants/responsorial_chant_phrase.py
@@ -10,6 +10,17 @@ class ResponsorialChantPhrase(BasePhrase):
 
     def __init__(self, measure_stream, *, piece):
         super().__init__(measure_stream, piece=piece)
+        self.is_last_phrase_in_stanza = self._has_double_or_final_barline()
+
+    def _has_double_or_final_barline(self):
+        """
+        Return True if this phrase ends in a double or final barline
+        (these indicate stanza boundaries in responsorial chant pieces).
+        """
+        barlines = list(self.measure_stream.getElementsByClass("Barline"))
+        assert len(barlines) <= 1
+        assert all([b.style in ("double", "final") for b in barlines])
+        return len(barlines) == 1
 
     def __repr__(self):
         return f"<Resp. Phrase {self.phrase_number} of piece {self.piece}>"
