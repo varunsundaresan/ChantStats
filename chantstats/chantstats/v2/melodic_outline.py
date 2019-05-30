8cb11ecdf78d6fcebcfcaa4202597a8c8e4fcd8b
Maximilian Albert <maximilian.albert@gmail.com>
2019-05-30 06:34:17 +0100

    Move melodic outline calculation into separate file

---
+++ chantstats/chantstats/v2/melodic_outline.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from music21.interval import Interval
+from .note_pair import NotePair
+
+__all__ = ["MelodicOutline", "has_framing_interval", "check_step_size"]
+
+
+def calculate_melodic_outline_candidates_for_phrase(phrase):
+    """
+    Parameters
+    ----------
+    phrase
+        A phrase in a plainchant sequence or responsorial chant piece.
+        Must have the attributes `notes` and `note_pairs`.
+    """
+    directions = pd.Series([note_pair.direction for note_pair in phrase.note_pairs])
+    directions_ffill = directions.replace(0, np.NaN).ffill(downcast="infer").bfill(downcast="infer")
+    dir_changes = directions_ffill.diff().fillna(0, downcast="infer") != 0
+    offsets_with_dir_changes = dir_changes[dir_changes != 0].index
+    mo_slices = list(zip(offsets_with_dir_changes, offsets_with_dir_changes[1:]))
+    mo_slices = [(0, offsets_with_dir_changes[0])] + mo_slices + [(offsets_with_dir_changes[-1], len(phrase.notes) + 1)]
+    mo_slices = [slice(i, j + 1) for (i, j) in mo_slices]
+    mo_candidates = [phrase.notes[sl] for sl in mo_slices]
+    return mo_candidates
+
+
+def get_melodic_outlines_from_candidates(melodic_outline_candidates, interval, *, allow_thirds=False):
+    """
+    Parameters
+    ----------
+    melodic_outline_candidates
+        List containing lists of notes, each of which represents a melodic outline candidate.
+    """
+    return [
+        MelodicOutline(notes)
+        for notes in melodic_outline_candidates
+        if has_framing_interval(notes, interval) and check_step_size(notes, allow_thirds)
+    ]
+
+
+def check_step_size(notes, allow_thirds, max_step_size=None):
+    max_step_size = max_step_size or (4 if allow_thirds else 2)
+    note_pairs = [NotePair(n1, n2) for n1, n2 in zip(notes, notes[1:])]
+    return all([note_pair.semitones <= max_step_size for note_pair in note_pairs])
+
+
+def has_framing_interval(notes, interval_name):
+    return Interval(notes[0], notes[-1]).name == interval_name
+
+
+class MelodicOutline:
+    def __init__(self, notes):
+        self.notes = notes
+        self.framing_note_pair = NotePair(self.notes[0], self.notes[-1])
+        self.framing_interval = self.framing_note_pair.interval
+        assert self.framing_interval.name in ["P4", "P5"]
+        self.bottom_pc = self.framing_note_pair.bottom_pc
+        self.top_pc = self.framing_note_pair.top_pc
+
+    def __repr__(self):
+        return f"<MO: pcs{self.bottom_pc}^{self.top_pc}_M{self.framing_interval.name[-1]}, {[n.nameWithOctave for n in self.notes]}>"
--- chantstats/chantstats/v2/plainchant_sequence_phrase.py
+++ chantstats/chantstats/v2/plainchant_sequence_phrase.py
@@ -1,36 +1,11 @@
 import music21
-import numpy as np
-import pandas as pd
-from music21.interval import Interval
 from .ambitus import calculate_ambitus
+from .melodic_outline import calculate_melodic_outline_candidates_for_phrase, get_melodic_outlines_from_candidates
 from .mode_degree import ModeDegree
 from .note_pair import NotePair
 from .pitch_class import PC
 
 __all__ = ["PlainchantSequencePhrase"]
-
-
-def check_step_size(notes, allow_thirds, max_step_size=None):
-    max_step_size = max_step_size or (4 if allow_thirds else 2)
-    note_pairs = [NotePair(n1, n2) for n1, n2 in zip(notes, notes[1:])]
-    return all([note_pair.semitones <= max_step_size for note_pair in note_pairs])
-
-
-def has_framing_interval(notes, interval_name):
-    return Interval(notes[0], notes[-1]).name == interval_name
-
-
-class MelodicOutline:
-    def __init__(self, notes):
-        self.notes = notes
-        self.framing_note_pair = NotePair(self.notes[0], self.notes[-1])
-        self.framing_interval = self.framing_note_pair.interval
-        assert self.framing_interval.name in ["P4", "P5"]
-        self.bottom_pc = self.framing_note_pair.bottom_pc
-        self.top_pc = self.framing_note_pair.top_pc
-
-    def __repr__(self):
-        return f"<MO: pcs{self.bottom_pc}^{self.top_pc}_M{self.framing_interval.name[-1]}, {[n.nameWithOctave for n in self.notes]}>"
 
 
 class PlainchantSequencePhrase:
@@ -56,27 +31,12 @@ class PlainchantSequencePhrase:
         self.pc_pairs = list(zip(self.pitch_classes, self.pitch_classes[1:]))
         self.note_pairs = [NotePair(n1, n2) for (n1, n2) in zip(self.notes, self.notes[1:])]
         self.mode_degree_pairs = list(zip(self.mode_degrees, self.mode_degrees[1:]))
-        self._melodic_outline_candidates = self._calculate_melodic_outline_candidates_for_phrase()
+        self._melodic_outline_candidates = calculate_melodic_outline_candidates_for_phrase(self)
 
     def __repr__(self):
         return f"<Phrase {self.phrase_number} of piece {self.piece}>"
 
-    def _calculate_melodic_outline_candidates_for_phrase(self):
-        directions = pd.Series([note_pair.direction for note_pair in self.note_pairs])
-        directions_ffill = directions.replace(0, np.NaN).ffill(downcast="infer").bfill(downcast="infer")
-        dir_changes = directions_ffill.diff().fillna(0, downcast="infer") != 0
-        offsets_with_dir_changes = dir_changes[dir_changes != 0].index
-        mo_slices = list(zip(offsets_with_dir_changes, offsets_with_dir_changes[1:]))
-        mo_slices = (
-            [(0, offsets_with_dir_changes[0])] + mo_slices + [(offsets_with_dir_changes[-1], len(self.notes) + 1)]
-        )
-        mo_slices = [slice(i, j + 1) for (i, j) in mo_slices]
-        mo_candidates = [self.notes[sl] for sl in mo_slices]
-        return mo_candidates
-
     def get_melodic_outlines(self, interval, *, allow_thirds=False):
-        return [
-            MelodicOutline(candidate)
-            for candidate in self._melodic_outline_candidates
-            if has_framing_interval(candidate, interval) and check_step_size(candidate, allow_thirds)
-        ]
+        return get_melodic_outlines_from_candidates(
+            self._melodic_outline_candidates, interval, allow_thirds=allow_thirds
+        )
