6f260cfb17f015cd71da7c5604893901dc02015e
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-03 00:48:43 +0100

    Allow calculating tendency for empty list of pairs

--- chantstats/chantstats/v2/tendency.py
+++ chantstats/chantstats/v2/tendency.py
@@ -1,25 +1,27 @@
 import pandas as pd
-from .note_pair import NotePair
+from .mode_degree import ModeDegree
+from .pitch_class import PC
 
 __all__ = ["BaseTendency", "PCTendency"]
 
 
 class BaseTendency:
-    def __init__(self, pairs, *, label_first, label_second):
-        first_items, second_items = zip(*pairs)
-        df = pd.DataFrame({label_first: first_items, label_second: second_items, "count": 1}).dropna()
-        cls_first = first_items[0].__class__
-        cls_second = second_items[0].__class__
-        df_pivot_table = df.pivot_table(
-            index=label_second, columns=label_first, values="count", aggfunc=sum, fill_value=0
-        )
-        self.df_pair_counts = (
-            df_pivot_table.reindex(cls_second.allowed_values)
-            .fillna(0, downcast="infer")
-            .reindex(cls_first.allowed_values, axis=1)
-            .fillna(0, downcast="infer")
-        )
-        # TODO: verify that we are not accidentally dropping values by reindexing (and add some safety measure for this if needed)
+    def __init__(self, pairs, *, label_first, label_second, cls_first, cls_second):
+        if pairs == []:
+            self.df_pair_counts = pd.DataFrame(0, columns=cls_first.allowed_values, index=cls_second.allowed_values)
+        else:
+            first_items, second_items = zip(*pairs)
+            df = pd.DataFrame({label_first: first_items, label_second: second_items, "count": 1}).dropna()
+            df_pivot_table = df.pivot_table(
+                index=label_second, columns=label_first, values="count", aggfunc=sum, fill_value=0
+            )
+            self.df_pair_counts = (
+                df_pivot_table.reindex(cls_second.allowed_values)
+                .fillna(0, downcast="infer")
+                .reindex(cls_first.allowed_values, axis=1)
+                .fillna(0, downcast="infer")
+            )
+            # TODO: verify that we are not accidentally dropping values by reindexing (and add some safety measure for this if needed)
 
         sums_per_col = self.df_pair_counts.sum(axis="index", skipna=False)
         self.df_condprobs_v1 = 100 * self.df_pair_counts.div(sums_per_col, axis="columns")  # each column sums up to 100
@@ -40,12 +42,14 @@ class BaseTendency:
 
 class PCTendency(BaseTendency):
     def __init__(cls, item):
-        super().__init__(item.pc_pairs, label_first="pc1", label_second="pc2")
+        super().__init__(item.pc_pairs, label_first="pc1", label_second="pc2", cls_first=PC, cls_second=PC)
 
 
 class ModeDegreeTendency(BaseTendency):
     def __init__(cls, item):
-        super().__init__(item.mode_degree_pairs, label_first="pc1", label_second="pc2")
+        super().__init__(
+            item.mode_degree_pairs, label_first="md1", label_second="md2", cls_first=ModeDegree, cls_second=ModeDegree
+        )
 
 
 # class PCApproaches(BaseTendency):
