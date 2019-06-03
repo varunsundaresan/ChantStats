3315fa56696b977c4b0042ff65cc951f996d2a23
Maximilian Albert <maximilian.albert@gmail.com>
2019-06-03 02:39:44 +0100

    Run all analyses

--- chantstats/tests/v2/calculate_and_export_results/run_analyses_and_export_result.py
+++ chantstats/tests/v2/calculate_and_export_results/run_analyses_and_export_result.py
@@ -16,8 +16,8 @@ def run_analyses_and_export_results(rep_and_genre, *, output_root_dir, analyses=
     min_num_notes_per_monomodal_section = 80
 
     pieces = load_pieces(rep_and_genre, cfg)
-    # analyses = analyses or ["pc_freqs", "tendency", "L_and_M__L5_u_M5", "L_and_M__L4_u_M4"]
-    analyses = analyses or ["pc_freqs"]
+    analyses = analyses or ["pc_freqs", "tendency", "L_and_M__L5_u_M5", "L_and_M__L4_u_M4"]
+
     if rep_and_genre in ("plainchant_sequences", "responsorial_chants"):
         modes = ["final", "final_and_ambitus"]
     elif rep_and_genre in ("organum_pieces", "organum_phrases"):
